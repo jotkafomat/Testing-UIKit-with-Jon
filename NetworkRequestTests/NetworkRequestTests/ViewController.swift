2a9fd2ebb213c534d5350672c8511929b47074c8
jotkafomat <k-j@xl.wp.pl>
2021-09-09 10:45:21 +0100

    isolate URLSession with protocol

--- NetworkRequestTests/NetworkRequestTests/ViewController.swift
+++ NetworkRequestTests/NetworkRequestTests/ViewController.swift
@@ -49,4 +49,9 @@ class ViewController: UIViewController {
     }
     
 }
+protocol URLSessionProtocol {
+    func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask
+}
+
+extension URLSession: URLSessionProtocol {}
 
