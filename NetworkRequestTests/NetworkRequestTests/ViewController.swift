6bcee07824083cf83c3947a1da6ad154c7a6e541
jotkafomat <k-j@xl.wp.pl>
2021-09-15 08:58:16 +0100

    Parsing response

--- NetworkRequestTests/NetworkRequestTests/ViewController.swift
+++ NetworkRequestTests/NetworkRequestTests/ViewController.swift
@@ -20,8 +20,13 @@ class ViewController: UIViewController {
     var session: URLSessionProtocol = URLSession.shared
     
     @IBAction private func buttonTapped() {
-        searchForBooks(terms: "out from bonnevile")
-
+        searchForBooks(terms: "out from boneville")
+    }
+    
+    private(set) var result: [SearchResult] = [] {
+        didSet {
+            print(result)
+        }
     }
     
     private func searchForBooks(terms: String) {
@@ -34,14 +39,31 @@ class ViewController: UIViewController {
         
         dataTask = session.dataTask(with: request) { [weak self] (data: Data?, response: URLResponse?, error: Error?) -> Void in
             guard let self = self else { return }
-            let decoded = String(data: data ?? Data(), encoding: .utf8)
             
-            print("response: \(String(describing: response))")
-            print("data: \(String(describing: decoded))")
-            print("error: \(String(describing: error))")
+            var decoded: Search?
+            var errorMessage: String?
             
+            if let error = error {
+                errorMessage = error.localizedDescription
+            } else if let response = response as? HTTPURLResponse, response.statusCode != 200 {
+                errorMessage = "Response" + HTTPURLResponse.localizedString(forStatusCode: response.statusCode)
+            } else if let data = data {
+                do {
+                    decoded = try JSONDecoder().decode(Search.self, from: data)
+                } catch {
+                    errorMessage = error.localizedDescription
+                }
+            }
+                    
             DispatchQueue.main.async { [weak self] in
                 guard let self = self else { return }
+                
+                if let decoded = decoded {
+                    self.result = decoded.results
+                }
+                if let errorMessage = errorMessage {
+                    self.showError(errorMessage)
+                }
                 self.dataTask = nil
                 self.button.isEnabled = true
             }
@@ -50,6 +72,22 @@ class ViewController: UIViewController {
         dataTask?.resume()
     }
     
+    private func showError(_ message: String) {
+        let title = "Network Problem"
+        print("\(title): \(message)")
+        
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(
+            title: "OK",
+            style: .default)
+        alert.addAction(okAction)
+        alert.preferredAction = okAction
+        present(alert, animated: true)
+    }
 }
 
 
@@ -58,4 +96,15 @@ protocol URLSessionProtocol {
 }
 
 extension URLSession: URLSessionProtocol {}
+
+struct Search: Decodable {
+    let results: [SearchResult]
+}
+
+struct SearchResult: Decodable, Equatable {
+    let artistName: String
+    let trackName: String
+    let averageUserRating: Float
+    let genres: [String]
+}
 
