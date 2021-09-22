230effe0927d5bb982ee1eb2d27a8652db074129
jotkafomat <k-j@xl.wp.pl>
2021-09-22 16:22:35 +0100

    passwordFieldAttributesShouldBeSet

--- TextField/ViewControllerTests/ViewControllerTests.swift
+++ TextField/ViewControllerTests/ViewControllerTests.swift
@@ -9,28 +9,48 @@ import XCTest
 @testable import TextField
 
 class ViewControllerTests: XCTestCase {
-
-    func test_outlets_shouldBeConnected() {
+    
+    private var sut: ViewController!
+    
+    override func setUp() {
+        super.setUp()
+        
         let sb = UIStoryboard(name: "Main", bundle: nil)
         
-        let sut: ViewController = sb.instantiateViewController(identifier: String(describing: ViewController.self))
+        sut = sb.instantiateViewController(identifier: String(describing: ViewController.self))
         
         sut.loadViewIfNeeded()
+    }
+    
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+
+    func test_outlets_shouldBeConnected() {
         
         XCTAssertNotNil(sut.userNameField, "userField")
         XCTAssertNotNil(sut.passwordField, "passwordField")
     }
     
     func test_usernameFieldAttributesShouldBeSet() {
-        let sb = UIStoryboard(name: "Main", bundle: nil)
-        
-        let sut: ViewController = sb.instantiateViewController(identifier: String(describing: ViewController.self))
-        
-        sut.loadViewIfNeeded()
-        
+     
+                
         let textField = sut.userNameField!
+        
         XCTAssertEqual(textField.textContentType, .username, "textContentType")
         XCTAssertEqual(textField.autocorrectionType, .no, "autocorrectionType")
-        XCTAssertEqual(textField.returnKeyType, .next, "returnKeyType   ")
+        XCTAssertEqual(textField.returnKeyType, .next, "returnKeyType")
+    }
+    
+    func test_passwordFieldAttributesShouldBeSet() {
+        
+        let textField = sut.passwordField!
+        
+        XCTAssertEqual(textField.textContentType, .password, "textContentType")
+        XCTAssertEqual(textField.returnKeyType, .go, "returnKeyType")
+        XCTAssertTrue(textField.isSecureTextEntry, "isSecureTextEntry")
+        
+        
     }
 }
